6d23d2a2b79d89e8d8f49f425724599f69786e40
PROPEL2580 <kumarishalini@ekaant.co>
2025-05-18 18:50:09 +0530

    pp

--- api/index.js
+++ api/index.js
@@ -33,7 +33,7 @@ let PORT = process.env.PORT || 3000;
 
 // Configure CORS before other middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://ekaant.onrender.com','https://employee-ekaant-co-9005-4d9445b1-wtjycodr.onporter.run'],
+  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://0.0.0.0:3000', 'https://ekaant.onrender.com','https://employee-ekaant-co-9005-4d9445b1-wtjycodr.onporter.run'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
