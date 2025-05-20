b5ea7585a3d2abe90b03a3a63af71deb4e336097
PROPEL2580 <kumarishalini@ekaant.co>
2025-05-21 00:03:26 +0530

    ss

--- employees/src/components/layout/Sidebar.jsx
+++ employees/src/components/layout/Sidebar.jsx
@@ -334,9 +334,10 @@ const Sidebar = () => {
           }}
         />
       ))}
+
            <div className="flex items-center justify-center gap-2 mb-6 py-4">
-        <img src="\logo-03.png" alt="Ekaant Logo" className="h-12 w-12 mb-6" />
-        <div className=" text-white text-4xl font-bold ml-4">EKAANT</div>
+        <img src="\logo-03.png" alt="Ekaant Logo" className="h-15 w-15" />
+        <div className="text-4xl text-white">EKAANT</div>
       </div>
       <div className="flex-2 bg-black/30 backdrop-blur-sm p-4 rounded-tr-[40px] rounded-tl-[40px] mt-6 py-4 px-4">
         <nav>
