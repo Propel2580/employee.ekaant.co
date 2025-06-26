b6f63af054038e212f1bcc69957b5a5d10cac23f
Propel2580 <kumarishalini@ekaant.co>
2025-06-26 15:51:44 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -31,14 +31,29 @@ const app = express();
 const __dirname = path.resolve();
 let PORT = process.env.PORT || 3000;
 
-// Configure CORS before other middleware
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://localhost:3000',
+  'http://0.0.0.0:3000',
+  'https://ekaant.onrender.com',
+  'https://employee-ekaant-co-9005-4d9445b1-wtjycodr.onporter.run',
+  'https://employee.ekaant.co'
+];
+
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://0.0.0.0:3000', 'https://ekaant.onrender.com','https://employee-ekaant-co-9005-4d9445b1-wtjycodr.onporter.run','https://employee.ekaant.co'],
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
   exposedHeaders: ['Content-Length']
 }));
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
